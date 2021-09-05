3b847e4563fcfe6df27ea7ca7a0d9469e915fca1
Samed <samedli.ssi@gmail.com>
2021-09-05 15:36:13 +0400

    Improvements

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layout')
 @section('content')
-<section>
-    <div class="container">
-        <div class="custom-table-wrapper">
-            <table class="table table-hover" id="d-table">
-                <thead>
+    <section>
+        <div class="container">
+            <div class="custom-table-wrapper">
+                <table class="table table-hover" id="d-table">
+                    <thead>
                     <tr>
                         <th>ID</th>
                         <th>Username</th>
@@ -15,29 +15,29 @@
                         <th>Fee</th>
                         <th></th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     @foreach($students as $student)
-                    <tr>
-                        <td>{{$student->id}}</td>
-                        <td>{{$student->username}}</td>
-                        <td>{{$student->mail}}</td>
-                        <td>{{$student->name}}</td>
-                        <td>{{$student->surname}}</td>
-                        <td>{{$student->group}}</td>
-                        <td>{{$student->fee}}</td>
-                        <td>
-                            <a href="/students/edit/{{$student->id}}" class="btn btn-primary">✎</a>
-                            <a href="/students/delete/{{$student->id}}" class="btn btn-danger">🗑</a>
-                        </td>
-                    </tr>
+                        <tr>
+                            <td>{{$student->id}}</td>
+                            <td>{{$student->username}}</td>
+                            <td>{{$student->mail}}</td>
+                            <td>{{$student->name}}</td>
+                            <td>{{$student->surname}}</td>
+                            <td>{{$student->group}}</td>
+                            <td>{{$student->fee}}</td>
+                            <td>
+                                <a href="/students/edit/{{$student->id}}" class="btn btn-primary">✎</a>
+                                <a href="/students/delete/{{$student->id}}" class="btn btn-danger">🗑</a>
+                            </td>
+                        </tr>
                     @endforeach
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
+            <p class="text-center mt-4">
+                <a href="/students/create" class="btn btn-success">Add Student</a>
+            </p>
         </div>
-        <p class="text-center mt-4">
-            <a href="/students/create" class="btn btn-success">Add Student</a>
-        </p>
-    </div>
-</section>
+    </section>
 @endsection
